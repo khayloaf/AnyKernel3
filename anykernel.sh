04583192affe654cfb23aa41b8d04b79a2e39d45
sahatchoky2-png T <sahatchoky2@gmail.com>
2025-09-12 14:17:13 +0000

    Add surya changes

--- anykernel.sh
+++ anykernel.sh
@@ -9,12 +9,12 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=munch
+device.name1=surya
 '; } # end properties
 
 # shell variables
 block="/dev/block/bootdevice/by-name/boot"
-is_slot_device=1
+is_slot_device=0
 ramdisk_compression=auto
 
 ## AnyKernel methods (DO NOT CHANGE)
@@ -26,31 +26,8 @@ ramdisk_compression=auto
 set_perm_recursive 0 0 750 750 "$ramdisk"/*
 set_perm_recursive 0 0 750 750 "$ramdisk"/init* "$ramdisk"/sbin
 
-# Auto-detect variant from zip name
-case "$ZIPFILE" in
-	*rethinking*) type=default;;
-	*miui*) type=miui;;
-esac
-
-# Automatic miui detection
-region="$(file_getprop /vendor/build.prop "ro.vendor.miui.build.region")"
-if [ -z "$region" ]; then region="$(file_getprop /product/etc/build.prop "ro.miui.build.region")"; fi
-
-case "$region" in
-	cn | in | ru | id | eu | tr | tw | gb | global | mx | jp | kr | lm | cl | mi)
-		type=miui
-		ui_print "  -> MIUI ROM is detected!"
-	;;
-esac
-
-# Select default if still unset
-[ -z "$type" ] && type=default
-
-# Apply the right dtbo
-ui_print " • Using $type DTBO"
-mv "${home}/kernels/${type}/dtbo.img" "${home}/dtbo.img";
-
-# Apply Image & dtb
+# Apply Image & dtbo
+mv "${home}/kernels/dtbo.img" "${home}/dtbo.img";
 mv "${home}/kernels/Image.gz-dtb" "${home}/Image.gz-dtb";
 
 ## AnyKernel install
